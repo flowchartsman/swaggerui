f32baceee632096dcd722d43d9e2debc4b7473bb
Andy Walker <walkeraj@gmail.com>
2021-01-18 01:17:03 -0500

    should use fs.Sub

--- swaggerui.go
+++ swaggerui.go
@@ -24,6 +24,7 @@ const (
 )
 
 type swagWrapFS struct {
+	static  fs.FS
 	overlay fstest.MapFS
 }
 
@@ -31,37 +32,22 @@ func (s *swagWrapFS) Open(name string) (fs.File, error) {
 	if _, err := s.overlay.Stat(name); err == nil {
 		return s.overlay.Open(name)
 	}
-	return swagfs.Open(name)
-}
-
-func (s *swagWrapFS) ReadFile(name string) ([]byte, error) {
-	if _, err := s.overlay.Stat(name); err == nil {
-		return s.overlay.ReadFile(name)
-	}
-	return swagfs.ReadFile(name)
-}
-
-// I don't know how important it is that I throw in a synthesized entry for the specfile, so for now I don't
-func (s *swagWrapFS) ReadDir(name string) ([]fs.DirEntry, error) {
-	return swagfs.ReadDir(name)
-}
-
-func addprefix(prefix string, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.URL.Path = prefix + r.URL.Path
-		next.ServeHTTP(w, r)
-	})
+	return s.static.Open(name)
 }
 
 // NewHandler returns a new handler that will serve swagger-ui with your spec
 func NewHandler(spec []byte, spectype SpecType) http.Handler {
 	//render the index template with the proper spec name inserted
-	tmpl, _ := template.ParseFS(swagfs, "embed/index.html")
+	static, _ := fs.Sub(swagfs, "embed")
+	tmpl, _ := template.ParseFS(static, "index.html")
 	idxbuf := new(bytes.Buffer)
 	tmpl.ExecuteTemplate(idxbuf, "index.html", struct{ SpecType string }{string(spectype)})
 	m := fstest.MapFS{
-		"embed/index.html":                  {Data: idxbuf.Bytes()},
-		"embed/swagger." + string(spectype): {Data: spec},
+		"index.html":                  {Data: idxbuf.Bytes()},
+		"swagger." + string(spectype): {Data: spec},
 	}
-	return addprefix("embed/", http.FileServer(http.FS(&swagWrapFS{overlay: m})))
+	return http.FileServer(http.FS(&swagWrapFS{
+		static:  static,
+		overlay: m,
+	}))
 }
